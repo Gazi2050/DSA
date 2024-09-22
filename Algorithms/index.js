0c664fed8a9da628c6804ff09b5a0c9ddb31937b
Gazi2050 <gazi45344@gmail.com>
2024-09-22 19:39:17 +0600

    countinue

---
+++ Algorithms/index.js
@@ -0,0 +1,14 @@
+function summmation(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(summmation(5));
+
+function sum(n) {
+    return (n * (n + 1)) / 2
+}
+console.log(sum(5));
---
+++ Data-Structures/Array/array.js
@@ -0,0 +1,6 @@
+const arr1 = new Array();
+const arr2 = Array();
+const arr3 = [];
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
