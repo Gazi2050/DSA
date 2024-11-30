bf0c61b35bd9cb2418b6424dac018b06a2385fb4
Gazi2050 <gazi45344@gmail.com>
2024-12-01 00:24:00 +0600

    Primitives types

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+/**Primitives: When you access a primitive type you work directly on its value.
+* string
+*number
+*boolean
+*null
+*undefined
+*symbol
+*bigint
+ */
+const foo = 1;
+let bar = foo;
+bar = 9;
+console.log(foo, bar)//=>1,9
