84477d80a9144fe6b00a17a788814a43f507a9c6
Gazi2050 <gazi45344@gmail.com>
2025-01-22 00:10:18 +0600

    print hello world

--- C++/index.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // Include the library for input and output
-
-int main()
-{
-    std::cout << "Hello world" << std::endl; // Print "Hello" followed by a newline
-    return 0;                                // Return 0 to indicate successful program termination
-}
