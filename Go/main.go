ce59c838ae7143ca52991aeb48839044c58103de
Gazi2050 <gazi45344@gmail.com>
2025-02-18 13:11:58 +0600

    variable in go

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+/*variable*/
+// Global variable declaration using `var` (Can be changed)
+var number = 100
+var singleCharacter = 'G'
+var name = "gazi"
+var bool = true
+
+// Constant declaration using `const` (Cannot be changed)
+const id = 9
+
 func main() {
-	fmt.Println("Hello World")
+	// Local variable declaration using `:=` (Shorthand syntax, inferred type)
+	funcVar := "hello world"
+	// Printing values
+	fmt.Println(number)
+	fmt.Println(singleCharacter)
+	fmt.Println(name)
+	fmt.Println(bool)
+	fmt.Println(id)
+	fmt.Println(funcVar)
 }
