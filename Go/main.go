2033f5ebd371c83eccea11dd99933e531d992849
Gazi2050 <gazi45344@gmail.com>
2025-02-20 18:23:49 +0600

    Operators in go

--- Go/main.go
+++ Go/main.go
@@ -35,21 +35,48 @@ var (
 	isFalse bool
 )
 
+/* Operators */
+// the type of keyword (var or const) used for values must match the keyword type used for the result to avoid errors in Go.
+var value1 int = 385
+var value2 int = 45
+var result int = value1 + value2
+var result2 int = value1 - value2
+var result3 int = value1 * value2
+var result4 int = value1 / value2
+
+const value4 int = 350
+const value5 int = 45
+const result5 int = value4 + value5
+
 func main() {
-	// Local variable declaration using shorthand `:=`
-	funcString := "hello world"
+	// cannot use the shorthand operator (:=,+=,-=,*=,%=,++,--) outside of a function in Go.
 	funcNumber := 99
-	funcBool := true
-
-	// Printing values
-	fmt.Println(funcString) // Print with a new line
+	funcNumber++
+	funcNumber--
+	num := 20
+	num += 10
+	num -= 5
+	num *= 2
+	num /= 5
+	num %= 3
+	fmt.Println(num)
+	fmt.Println(result)
+	fmt.Println(result2)
+	fmt.Println(result3)
+	fmt.Println(result4)
+	fmt.Println(result5)
 	fmt.Println(funcNumber)
-
+	// Local variable declaration using shorthand `:=`
+	// funcString := "hello world"
+	// funcBool := true
+	// Printing values
+	// fmt.Println(funcString) // Print with a new line
+	// fmt.Println(funcNumber)
 	// Print in the same line
-	fmt.Print(funcBool)
+	// fmt.Print(funcBool)
 	//Print in the same line in Printf()
-	fmt.Printf("%s", funcString)  // ✅ Correct (prints a string)
-	fmt.Printf("%2d", funcNumber) // ✅ Corrected (uses %d for integers)
-	fmt.Printf("%t", funcBool)    // ✅ Corrected (uses %t for boolean values)
+	// fmt.Printf("%s", funcString)  // ✅ Correct (prints a string)
+	// fmt.Printf("%2d", funcNumber) // ✅ Corrected (uses %d for integers)
+	// fmt.Printf("%t", funcBool)    // ✅ Corrected (uses %t for boolean values)
 
 }
