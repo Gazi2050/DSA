9f3d7339ae6514d5704a96e82544ca584e2db280
Gazi2050 <gazi45344@gmail.com>
2025-02-21 23:43:29 +0600

    string index

--- Go/str.go
+++ Go/str.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	var str string = "A String"
 	var LongStr string = "A Long String"
+	var strIndex = "String"
 	fmt.Println(str)
 	fmt.Println(LongStr)
+	fmt.Printf("%c", strIndex[0])
 }
