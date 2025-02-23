937c0fceff29bc36936022ccd69dd0a86363ddc0
Gazi2050 <gazi45344@gmail.com>
2025-02-23 13:41:48 +0600

    string Searching & Checking

--- Go/str.go
+++ Go/str.go
@@ -8,9 +8,16 @@ import (
 func main() {
 	var str string = "Hello world"
 	// var LongStr string = "A Long String"
-	var strIndex = "String"
-	fmt.Printf("%c", strIndex[0])
-	fmt.Println(len(str))
-	fmt.Println(strings.Contains(str, "world"))
+	// var strIndex = "String"
+	// fmt.Printf("%c", strIndex[0])
+	// fmt.Println(len(str))
+	// fmt.Println(strings.Contains(str, "world"))
+	// fmt.Println(strings.ContainsAny(str, "asn"))
+	// fmt.Println(strings.HasPrefix(str, "He"))
+	// fmt.Println(strings.HasSuffix(str, "ld"))
+	// fmt.Println(strings.Index(str, "d"))
+	// fmt.Println(strings.IndexAny(str, "asd"))
+	fmt.Println(strings.LastIndex(str, "l"))
+	// fmt.Println(strings.ToLower(str))
 
 }
