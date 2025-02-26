c960e9214d0b5955a717bb7e573dcb3b6f1d13d3
Gazi2050 <gazi45344@gmail.com>
2025-02-26 23:53:31 +0600

    learing array

--- Go/array.go
+++ Go/array.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
+//In Go, arrays cannot be constants. The const keyword in Go is used for compile-time constants, and arrays are not allowed to be constants because their size or elements can vary at runtime.Arrays hold values that can be modified, so Go does not allow arrays to be constants.
 var arr [5]int
-var arr2 = [...]int{1, 2, 3, 4, 5}
+var arr2 = [5]int{1, 2, 3, 4, 5}
+var arr3 = [...]string{"a", "b", "c"}
 
 func main() {
 	a := [5]int{1, 2, 3, 4, 5}
 	b := [...]int{10, 20, 30}
+
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(arr)
 	fmt.Println(arr2)
+	fmt.Println(arr3)
 }
