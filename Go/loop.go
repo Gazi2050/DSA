24c039803606e4fc1a2ad9b165d74c7b53f09742
Gazi2050 <gazi45344@gmail.com>
2025-02-27 21:18:06 +0600

    learning loop in go

--- Go/loop.go
+++ Go/loop.go
@@ -5,7 +5,30 @@ import "fmt"
 //Go has only one looping construct: the for loop. Unlike other languages that have while and do-while, Go simplifies iteration by handling all looping needs with for.
 
 func main() {
-	for i := 1; i < 5; i++ {
-		fmt.Println(i)
+	/*normal for loop*/
+	// for i := 1; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	/*Infinite loop*/
+	// for {
+	// 	fmt.Println("this is Infinite loop")
+	// }
+
+	/*for as a while Loop*/
+	// j := 1
+	// for j < 5 {
+	// 	fmt.Println(j)
+	// 	j++
+	// }
+
+	/*for as a do-while Loop*/
+	k := 1
+	for {
+		fmt.Println("count:", k)
+		k++
+		if k > 10 {
+			break
+		}
 	}
 }
