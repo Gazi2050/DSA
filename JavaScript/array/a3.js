3c896a24dc8394c914d8d1407f6b06c785ecd1a7
Gazi2050 <gazi45344@gmail.com>
2025-03-17 22:59:54 +0600

    Searching Methods

--- JavaScript/array/a3.js
+++ JavaScript/array/a3.js
@@ -1,2 +1,17 @@
 //Searching Methods
-var a
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const index = arr.indexOf(2);
+const index2 = arr.indexOf(22);
+console.log(index);
+console.log(index2);
+
+const isExist = arr.includes(7)
+const isExist2 = arr.includes(77)
+console.log(isExist);
+console.log(isExist2);
+
+const find1 = arr.find(e => e > 5)
+const find2 = arr.findIndex(e => e > 5)
+console.log(find1);
+console.log(find2);
