9d340e2ebc009ce8b28ab793c5ccca04bf7ff0cf
Gazi2050 <gazi45344@gmail.com>
2025-06-03 21:03:57 +0600

    add sumAllElement.ts

---
+++ JavaScript/array/sumAllElement.ts
@@ -0,0 +1,9 @@
+const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum: number = 0
+
+//solution-01
+//Using for loop
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[1]
+}
+console.log(sum);
