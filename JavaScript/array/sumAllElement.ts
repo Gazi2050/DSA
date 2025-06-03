7586c6a33a77728cfb765f23367838d1fe5b8ea9
Gazi2050 <gazi45344@gmail.com>
2025-06-03 22:35:45 +0600

    add 3 solution

--- JavaScript/array/sumAllElement.ts
+++ JavaScript/array/sumAllElement.ts
@@ -1,9 +1,40 @@
 const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let sum: number = 0
 
 //solution-01
 //Using for loop
-for (let i = 0; i < arr.length; i++) {
-    sum += arr[1]
+function sumByLoop(array: number[]) {
+    let sum: number = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += arr[i]
+    }
+    console.log(sum);
 }
-console.log(sum);
+// sumByLoop(arr)
+
+//solution-02
+//Using forEach() Method
+function sumByForEach(array: number[]) {
+    let sum = 0;
+    array.forEach(num => sum += num);
+    console.log(sum);
+}
+
+// sumByForEach(arr)
+
+//solution-03
+//Using Recursion
+function sumByRecursion(array: number[], idx: number): number {
+    if (idx === array.length) {
+        return 0;
+    }
+
+    const sum = array[idx] + sumByRecursion(array, idx + 1);
+
+    if (idx === 0) {
+        console.log(sum);
+    }
+
+    return sum;
+}
+
+sumByRecursion(arr, 0)
